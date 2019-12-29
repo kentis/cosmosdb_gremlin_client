7162ef358a672e4086d1ccb9b64afb75e354b564
kent <kentis@users.noreply.github.com>
2019-12-29 16:02:14 +0100

    fix requires

--- lib/gremlin_client.rb
+++ lib/gremlin_client.rb
@@ -4,9 +4,9 @@ require 'json'
 require 'pathname'
 require 'websocket-client-simple'
 
-require './gremlin_client/server_error.rb'
-require './gremlin_client/connection_timeout_error.rb'
-require './gremlin_client/execution_timeout_error.rb'
-require './gremlin_client/connection.rb'
+require 'gremlin_client/server_error'
+require 'gremlin_client/connection_timeout_error'
+require 'gremlin_client/execution_timeout_error'
+require 'gremlin_client/connection'
 module GremlinClient
 end
